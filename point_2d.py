5292db9f560e28f4d75ba33f2221df258dca0cd2
Xin Yu <scaner@gmail.com>
2022-02-15 23:50:55 +0800

    slogo processor 1.

---
+++ point_2d.py
@@ -0,0 +1,90 @@
+__version__ = '0.0.1'
+
+def extract_xy(*args):
+    if len(args) == 1 and (type(args[0]) == int or type(args[0]) == float):
+        return (args[0], args[0])
+
+    if len(args) == 2:
+        return (args[0], args[1])
+
+    arg = args[0]
+    if type(arg) == list or type(arg) == tuple:
+        return arg[:2]
+
+    if type(arg) == Point2D:
+        return arg
+    return args[:2]
+
+
+class Point2D(tuple):
+    def __new__(self, *args):
+        x, y = extract_xy(*args)
+        return tuple.__new__(Point2D, (x, y))
+
+    def __mul__(self, p):
+        x, y = extract_xy(p)
+        return Point2D(self[0] * x, self[1] * y)
+    
+    def __add__(self, p):
+        x, y = extract_xy(p)
+        return Point2D(self[0] + x, self[1] + y)
+    
+    def __truediv__(self, p):
+        x, y = extract_xy(p)
+        return Point2D(self[0] / x, self[1] / y)
+    
+    def __sub__(self, p):
+        x, y = extract_xy(p)
+        return Point2D(self[0] - x, self[1] - y)
+
+    def center_extend(self, *p):
+        r = Rect(self, self)
+        return r.center_extend(*p)
+
+    def extend(self, *p):
+        r = Rect(self, self)
+        return r.extend(*p)
+
+
+class Rect(tuple):
+    def __new__(self, P0, P1):
+        return tuple.__new__(Rect, (P0, P1))
+
+    def move(self, P):
+        self.P0 += P
+        self.P1.add(P)
+        return self
+
+    def extend(self, *p):
+        P = Point2D(*p)
+        return Rect(self[0], self[1] + P)
+
+    def center_extend(self, *p):
+        P =Point2D(*p) / 2
+        return Rect(self[0] - P, self[1] + P)
+
+    # def __repr__(self):
+    #     return f'Rect({self.P0.x},{self.P0.y},{self.P1.x},{self.P1.y})'
+
+
+P2 = Point2D
+R = Rect
+
+if __name__ == '__main__':
+    P0 = Point2D(10, 20)
+    print(P0)
+    P1 = P0 * 2
+    print(P1)
+    P2 = P1 - 5
+    print(P2)
+    # print(P0.add(5))
+    # P0.sub(30, 30)
+    # print(P0)
+    # r = P0.center_extend(10, 10)
+    # print(r)
+    # P1 = Point2D(P0)
+    # print(P1)
+    # P3 = P1 + P0
+    # print(len(P3))
+    # x, y = P3
+    # print(x, y)
--- resize_1.py
+++ resize_1.py
@@ -1,9 +1,13 @@
 import os
+import sys
 import glob
 
-from PIL import Image, ImageDraw
+
+from PIL import Image, ImageDraw, ImageFont
+from point_2d import Point2D as P, Rect as R
 
 IMG_DIR = '/Users/is/P3/JPEG'
+FONT_0_FN = 'font/ubuntu/Ubuntu-C.ttf'
 # fn = '/Users/is/P3/JPEG/A_02189__20220210_133505.jpg'
 
 
@@ -40,7 +44,6 @@ def calc_size(shape, max_size):
 # --
 def border(img, border_width=5, border_color=(255, 255, 255, 128)):
     background = img.convert('RGBA')
-
     border =  Image.new('RGBA', img.size, border_color)
     draw = ImageDraw.Draw(border)
     w, h = img.size
@@ -49,20 +52,75 @@ def border(img, border_width=5, border_color=(255, 255, 255, 128)):
     out = Image.alpha_composite(background, border)
     return out.convert('RGB')
 
+
+
+IS_STYLE_DEFAULT_CFG = {
+    'text': 'IS',
+    'gain': 5,
+    'logo_offset': (50, 50),
+    'font_size': 45,
+    'bottum_width': 10,
+    'bgcolor': (255, 255, 255, 128),
+    'fgcolor': (32, 32, 32, 16),
+    'radius': 35,
+    'padding': 15,
+    'paddingx': 8,
+}
+
+def is_style_0(img, **kwargs):
+    C = dict(IS_STYLE_DEFAULT_CFG)
+    C.update(kwargs)
+
+    text = C['text']
+    g = C['gain']
+    bg = C['bgcolor']
+    fg = C['fgcolor']
+
+    base_size = P(img.size)
+    draw_size = base_size * g
+    l0 = Image.new('RGBA', draw_size, (0,0,0,0))
+    draw = ImageDraw.Draw(l0)
+    font_0 = ImageFont.truetype(
+        FONT_0_FN, C['font_size'] * g, 
+        layout_engine=ImageFont.LAYOUT_RAQM)
+    font_size = draw.textsize(text, font = font_0)
+    font_size = P(font_size)
+    base_offset = P(C['logo_offset']) * g
+    base_center = draw_size - base_offset
+    r = base_center.center_extend(font_size)
+    r = r.center_extend(P(C['padding']) * g).center_extend(P(C['paddingx'], 0) * g)
+    draw.rounded_rectangle(r, fill=bg, radius=C['radius']*g)
+    draw.text(base_center, text,
+        anchor='mm', font=font_0, fill=fg)
+    p1 = P(0, draw_size[1] - C['bottum_width'] * g)
+    draw.rectangle((p1, draw_size- (1,1)), fill=bg)
+    l0 = l0.resize(img.size, resample=Image.LANCZOS)
+    img = Image.alpha_composite(img, l0)
+    return img
+
+
 def process(infn, outfn):
     im = Image.open(infn)
     new_size = calc_size(im.size, 1920)
-    out = im.resize(new_size, resample=Image.LANCZOS)
-    # out = border(out, 16, border_color=(255, 255, 255, 239))
     print(f'''{infn} -> {os.path.basename(outfn)} : {im.size} -> {new_size}''')
+    out = im.resize(new_size, resample=Image.LANCZOS)
+    out = out.convert('RGBA')
+    out = is_style_0(out, text='IS')
+    out = out.convert('RGB')
     out.save(outfn, quality=100, subsampling=0)
+
 
 # ---
 def main():
-    imgs = gen_image_list(IMG_DIR)
+    img_dir = IMG_DIR
+    if len(sys.argv) > 1:
+        img_dir = sys.argv[1]
+    imgs = gen_image_list(img_dir)
     for pair in imgs:
         process(pair[0], pair[1])
 
+def main1():
+    process('DSC01952.jpg', 'out.jpg')
 
 if __name__ == '__main__':
     main()
