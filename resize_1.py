9130aabba0233aede5348f37988a8be3d0f005b4
Yu Xin <scaner@gmail.com>
2022-02-17 14:12:00 +0800

    auto light detect.

--- resize_1.py
+++ resize_1.py
@@ -2,7 +2,7 @@ import os
 import sys
 import glob
 
-
+import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from point_2d import Point2D as P, Rect as R
 
@@ -67,7 +67,7 @@ IS_STYLE_DEFAULT_CFG = {
     'paddingx': 8,
 }
 
-def is_style_0(img, **kwargs):
+def watermark_is_0(img, **kwargs):
     C = dict(IS_STYLE_DEFAULT_CFG)
     C.update(kwargs)
 
@@ -99,6 +99,55 @@ def is_style_0(img, **kwargs):
     return img
 
 
+def sub_rect_light(img, rect):
+    im_corp = img.crop((rect[0][0], rect[0][1], rect[1][0], rect[1][1]))
+    im_gray = im_corp.convert('L')
+    return int(np.mean(np.asarray(im_gray)))
+
+
+def watermark_is_1(img, **kwargs):
+    C = dict(IS_STYLE_DEFAULT_CFG)
+    C.update(kwargs)
+
+    text = C['text']
+    g = C['gain']
+    bg = C['bgcolor']
+    fg = C['fgcolor']
+
+    base_size = P(img.size)
+    draw_size = base_size * g
+    l0 = Image.new('RGBA', draw_size, (0,0,0,0))
+    draw = ImageDraw.Draw(l0)
+    font_0 = ImageFont.truetype(
+        FONT_0_FN, C['font_size'] * g, 
+        layout_engine=ImageFont.LAYOUT_RAQM)
+    font_size = draw.textsize(text, font = font_0)
+    font_size = P(font_size)
+    base_offset = P(C['logo_offset']) * g
+    base_center = draw_size - base_offset
+    r = base_center.center_extend(font_size)
+    r = r.center_extend(P(C['padding']) * g).center_extend(P(C['paddingx'], 0) * g)
+
+    r0 = R(r[0] / g, r[1] / g)
+    #print('light')
+    #print(sub_rect_light(img, r0))
+    bg_light = sub_rect_light(img, r0)
+    print(bg_light)
+    border_color = bg
+    if bg_light >= 190:
+        bg = (255-bg[0], 255-bg[1], 255-bg[2], bg[3])
+        fg = (255-fg[0], 255-fg[1], 255-fg[2], fg[3])
+
+    draw.rounded_rectangle(r, fill=bg, radius=C['radius']*g)
+    draw.text(base_center, text,
+        anchor='mm', font=font_0, fill=fg)
+    p1 = P(0, draw_size[1] - C['bottum_width'] * g)
+    draw.rectangle((p1, draw_size- (1,1)), fill=border_color)
+    l0 = l0.resize(img.size, resample=Image.LANCZOS)
+    img = Image.alpha_composite(img, l0)
+    return img
+
+
 def process(infn, outfn):
     im = Image.open(infn)
     exif = im.info['exif']
@@ -106,7 +155,7 @@ def process(infn, outfn):
     print(f'''{infn} -> {os.path.basename(outfn)} : {im.size} -> {new_size}''')
     out = im.resize(new_size, resample=Image.LANCZOS)
     out = out.convert('RGBA')
-    out = is_style_0(out, text='IS')
+    out = watermark_is_1(out, text='IS')
     out = out.convert('RGB')
     out.save(outfn, quality=100, subsampling=0, exif=exif)
 
