d60357a4c908ad883f51a0828cc35b975c29b952
Yu Xin <scaner@gmail.com>
2022-03-16 19:38:51 +0800

    dump multiple pages.

--- ins/get_user_facebook_pages.py
+++ ins/get_user_facebook_pages.py
@@ -13,10 +13,11 @@ params = getCreds() # get creds
 params['debug'] = 'no' # set debug
 response = getUserPages( params ) # get debug info
 
-print ("\n---- FACEBOOK PAGE INFO ----\n") # section heading
-print ("Page Name:") # label
-print (response['json_data']['data'][0]['name']) # display name
-print ("\nPage Category:") # label
-print (response['json_data']['data'][0]['category']) # display category
-print ("\nPage Id:") # label
-print (response['json_data']['data'][0]['id']) # display id
+for i in range(len(response['json_data']['data'])):
+	print ("\n---- FACEBOOK PAGE INFO %d ----\n" % i) # section heading
+	print ("Page Name:") # label
+	print (response['json_data']['data'][i]['name']) # display name
+	print ("\nPage Category:") # label
+	print (response['json_data']['data'][i]['category']) # display category
+	print ("\nPage Id:") # label
+	print (response['json_data']['data'][i]['id']) # display id
