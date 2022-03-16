ae25b07d73208eac8529567df6f0004036471d13
Xin Yu <scaner@gmail.com>
2022-03-16 23:31:12 +0800

    update post script.

--- ins/post__0.py
+++ ins/post__0.py
@@ -6,7 +6,6 @@ from defines import getCreds, makeApiCall, makeApiPost
 
 """
 https://developers.facebook.com/docs/instagram-api/guides/content-publishing
-
 https://developers.facebook.com/docs/instagram-api/reference/ig-user/media#creating
 """
 
@@ -19,7 +18,7 @@ def get_quota(params):
     return resp['json_data']['data'][0]['quota_usage']
 
 
-def post_photo(params, post):
+def create_container(params, post):
     url = params['endpoint_base'] + params['instagram_account_id'] + "/media"
     endpoint_params = {}
     endpoint_params['access_token'] = params['access_token']
@@ -28,12 +27,32 @@ def post_photo(params, post):
     if 'location_id' in post:
         endpoint_params['location_id'] = post['location_id']
     resp = makeApiPost(url, endpoint_params, params['debug'])
-    return resp['json_data']
+    return resp
+
+
+def publish_container(params, container_id):
+    url = params['endpoint_base'] + params['instagram_account_id'] + "/media_publish"
+    endpoint_params = {}
+    endpoint_params['access_token'] = params['access_token']
+    endpoint_params['creation_id'] = container_id
+    resp = makeApiPost(url, endpoint_params, params['debug'])
+    return resp
+
 
 def main():
     params = getCreds()
     params['debug'] = 'yes'
     print(get_quota(params))
+
+    post = {
+        'image_url': 'https://raw.githubusercontent.com/is-attic/P0/a1b49ee1169c3cad642cd5838597a24a556efbbd/A_04226__20220220_100213__1.jpg',
+        'caption': 'test__0123 #scenery #native'
+    }
+    
+    resp = create_container(params, post)
+    id = resp['json_data']['id']
+    resp2 = publish_container(params, id)
+
 
 
 if __name__ == '__main__':
