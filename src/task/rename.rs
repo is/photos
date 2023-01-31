fa3c5bae4b5fec3090a28c392997445e6c56f448
Yu Xin <scaner@gmail.com>
2023-02-01 00:33:58 +0800

    dir walker in rename.

--- src/task/rename.rs
+++ src/task/rename.rs
@@ -6,18 +6,24 @@ use walkdir::WalkDir;
 pub enum RenameError {
     #[error("io-error {0}: {1}")]
     Io(String, String),
-    #[error("OK")]
-    Ok,
 }
 
 impl From<walkdir::Error> for RenameError {
     fn from(i: walkdir::Error) -> Self {
-        RenameError::Io(String::from("walkdir"), i.to_string())
+        Self::Io(String::from("walkdir"), i.to_string())
+    }
+}
+
+impl From<crate::core::fninfo::InfoErr> for RenameError {
+    fn from(e: crate::core::fninfo::InfoErr) -> Self {
+        Self::Io(String::from("metainfo"),
+            e.to_string())
     }
 }
 
 pub struct Request {
     pub dir: String,
+    pub exif: bool,
     pub dry: bool,
 }
 
@@ -25,7 +31,8 @@ impl Request {
     pub fn from(item: &crate::RenameCommand) -> Self {
         Request {
             dir: item.dir.clone(),
-            dry: item.dry.clone(),
+            exif: item.exif,
+            dry: item.dry,
         }
     }
 }
@@ -58,7 +65,35 @@ fn do_walk<T: AsRef<Path>>(req: &Request, level: i32, dir: T) -> Result<(), Rena
     let mut name_map : HashMap<String, String> = HashMap::new();
     
     for entry in files {
-        println!("{} files - {}", level, entry.path().to_str().unwrap());
+        let path = entry.path();
+        let full_path = path.to_str().unwrap().to_string();
+        let file_name = path.file_name().unwrap().to_str().unwrap().to_string();
+        let file_stem = path.file_stem().unwrap().to_str().unwrap().to_string();
+        let file_ext = path.extension();
+
+        if file_ext.is_none() {
+            continue;
+        }
+        let file_ext = file_ext.unwrap().to_str().unwrap().to_string();
+        let file_ext_lower = file_ext.to_ascii_lowercase();
+
+        let is_img = crate::utils::is_img_ext(file_ext_lower);
+
+        if !is_img {
+            println!("{level} - {full_path:?} - NO.IMG");
+            continue
+        }
+        
+        let meta = crate::core::fninfo::from(&full_path);
+        if meta.is_err() {
+            println!("{level} - {full_path:?} - MISS.META");
+            continue;
+        }
+
+        let meta = meta.unwrap();
+        println!("{level} - {full_path:?} -> {}.{}",
+            meta.to_name(), file_ext)
+        
     }
     Ok(())
 }
